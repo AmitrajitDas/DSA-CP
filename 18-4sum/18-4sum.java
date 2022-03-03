8d440f83ed030ec42702b8e4f632f0e888c6a900
Amitrajit Das <60144525+AmitrajitDas@users.noreply.github.com>
2022-03-03 22:37:29 +0530

    Time: 20 ms (72.92%), Space: 44.6 MB (45.99%) - LeetHub

--- 18-4sum/18-4sum.java
+++ 18-4sum/18-4sum.java
@@ -11,7 +11,7 @@ class Solution {
     
         Arrays.sort(num); 
     
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n - 1; i++) {
         
             int target_3 = target - num[i];
         
