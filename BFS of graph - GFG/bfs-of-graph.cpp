1ec0500798e577d75c0f014567e1f1f896e75b24
Amitrajit Das <60144525+AmitrajitDas@users.noreply.github.com>
2022-04-24 13:31:23 +0530

    Added solution - LeetHub

--- BFS of graph - GFG/bfs-of-graph.cpp
+++ BFS of graph - GFG/bfs-of-graph.cpp
@@ -6,44 +6,35 @@ using namespace std;
 class Solution {
   public:
   
-//   void bfs(int node, vector<int> &vis, vector<int> &res, vector<int> adj[]) {
-//       queue<int> q;
-//       q.push(node);
-//       vis[node] = 1;
-//       while(!q.empty()) {
-//           int temp = q.front();
-//           q.pop();
-//           res.push_back(temp);
-//           for(auto it : adj[node]) {
-//               if(vis[it] == 0) {
-//                   q.push(it);
-//                   vis[it] = 1;
-//               }
-//           }
-//       }
-//   }
+  void bfs(vector<int> &vis, vector<int> &res, vector<int> adj[], queue<int> &q) {
+      
+      while(!q.empty()) {
+          int temp = q.front();
+          q.pop();
+          res.push_back(temp);
+          for(auto it : adj[temp]) {
+              if(vis[it] == 0) {
+                  q.push(it);
+                  vis[it] = 1;
+              }
+          }
+      }
+  }
   
     // Function to return Breadth First Traversal of given graph.
     vector<int> bfsOfGraph(int V, vector<int> adj[]) {
-       vector < int > bfs;
-      vector < int > vis(V, 0);
-      queue < int > q;
-      q.push(0);
-      vis[0] = 1;
-      while (!q.empty()) {
-        int node = q.front();
-        q.pop();
-        bfs.push_back(node);
-
-        for (auto it: adj[node]) {
-          if (!vis[it]) {
-            q.push(it);
-            vis[it] = 1;
-          }
-        }
-      }
-
-      return bfs;
+        vector<int> vis(V+1, 0);
+        vector<int> res;
+        queue<int> q;
+        q.push(0);
+        vis[0] = 1;
+        
+            
+                bfs(vis, res, adj, q);
+            
+        
+        
+        return res;
     }
 };
 
