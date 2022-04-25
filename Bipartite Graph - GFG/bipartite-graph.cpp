9bfa02c8378603a230873172f48cc6cc105ab999
Amitrajit Das <60144525+AmitrajitDas@users.noreply.github.com>
2022-04-25 19:34:52 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -4,40 +4,40 @@ using namespace std;
 
  // } Driver Code Ends
 class Solution {
-    
-    bool bipartiteBfs(int src, vector<int> adj[], int color[]) {
-    queue<int>q;
-    q.push(src); 
-    color[src] = 1; 
-    while(!q.empty()) {
-        int node = q.front(); 
-        q.pop();
-        
-        for(auto it : adj[node]) {
-            if(color[it] == -1) {
-                color[it] = 1 - color[node]; 
-                q.push(it); 
-            } else if(color[it] == color[node]) {
-                return false; 
-            }
-        }
-    }
-    return true; 
-}
 
-public:
-bool isBipartite(int V, vector<int> adj[]) {
-    int color[V];
-    memset(color, -1, sizeof color); 
-    for(int i = 0;i<V;i++) {
-        if(color[i] == -1) {
-            if(!bipartiteBfs(i, adj, color)) {
-                return false;
-            }
+  bool bipartiteBfs(int src, vector < int > adj[], int color[]) {
+    queue < int > q;
+    q.push(src);
+    color[src] = 1;
+    while (!q.empty()) {
+      int node = q.front();
+      q.pop();
+
+      for (auto it: adj[node]) {
+        if (color[it] == -1) {
+          color[it] = 1 - color[node];
+          q.push(it);
+        } else if (color[it] == color[node]) {
+          return false;
         }
+      }
     }
-    return true; 
-}
+    return true;
+  }
+
+  public:
+    bool isBipartite(int V, vector < int > adj[]) {
+      int color[V];
+      memset(color, -1, sizeof color);
+      for (int i = 0; i < V; i++) {
+        if (color[i] == -1) {
+          if (!bipartiteBfs(i, adj, color)) {
+            return false;
+          }
+        }
+      }
+      return true;
+    }
 };
 
 // { Driver Code Starts.
