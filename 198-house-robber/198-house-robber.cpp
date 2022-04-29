a9e5b909d5ef34d89f221cfe83c318d97bd1f008
Amitrajit Das <amitrajitdas31@gmail.com>
2022-04-29 18:04:09 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (95.19%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,20 +1,23 @@
+////////////////// SPACE OPTIMIZATION /////////////////
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1);
-        dp[0] = nums[0];
+        int prev = nums[0], prev2 = 0;
         
         for(int i = 1; i < n; i++) {
             
             int pick = nums[i];
-            if(i > 1) pick += dp[i-2];
-            int notPick = 0 + dp[i-1];
+            if(i > 1) pick += prev2;
+            int notPick = 0 + prev;
             
-            dp[i] = max(pick, notPick);
+            int curr = max(pick, notPick);
+            prev2 = prev;
+            prev = curr;
         }
         
-        return dp[n-1];
+        return prev;
     }
 };
 
