15eb548ca8afc3d6be7677321b1452c53d4311a2
Amitrajit Das <amitrajitdas31@gmail.com>
2022-05-14 17:55:18 +0530

    Time: 5 ms (14.98%), Space: 6.6 MB (26.46%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,24 +1,52 @@
 class Solution {
 public:
-    
-    int rec(int i, int j, vector<vector<int>> &dp) {
-        
-        if(i == 0 && j == 0) return 1;
-        if(i < 0 || j < 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int up = rec(i - 1, j, dp);
-        int left = rec(i, j - 1, dp);
-        
-        return dp[i][j] = up + left;
-    }
-    
     int uniquePaths(int m, int n) {
         
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return rec(m - 1, n - 1, dp);
+        vector<vector<int>> dp(m,vector<int>(n));
+        dp[0][0] = 1;
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 && j == 0) dp[i][j] = 1;
+                else {
+                    int up = 0, left = 0;
+                    
+                    if(i > 0) up = dp[i - 1][j];
+                    if(j > 0) left = dp[i][j - 1];
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        
+        return dp[m-1][n-1];
     }
 };
+
+
+///// MEMOIZATION /////
+
+// class Solution {
+// public:
+    
+//     int rec(int i, int j, vector<vector<int>> &dp) {
+        
+//         if(i == 0 && j == 0) return 1;
+//         if(i < 0 || j < 0) return 0;
+//         if(dp[i][j] != -1) return dp[i][j];
+        
+//         int up = rec(i - 1, j, dp);
+//         int left = rec(i, j - 1, dp);
+        
+//         return dp[i][j] = up + left;
+//     }
+    
+//     int uniquePaths(int m, int n) {
+        
+//         vector<vector<int>> dp(m,vector<int>(n,-1));
+//         return rec(m - 1, n - 1, dp);
+//     }
+// };
+
 
 /////// RECURSION ///////
 
