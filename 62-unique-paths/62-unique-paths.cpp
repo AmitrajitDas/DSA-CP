91dab0947e165c246f6c0d85ff6238cd0a54d51a
Amitrajit Das <amitrajitdas31@gmail.com>
2022-05-14 18:25:17 +0530

    Time: 3 ms (38.48%), Space: 6.2 MB (57.90%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -2,26 +2,59 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         
-        vector<vector<int>> dp(m,vector<int>(n));
-        dp[0][0] = 1;
+        vector<int> prev(n, 0);
         
         for(int i = 0; i < m; i++) {
+            
+            vector<int> curr(n, 0);
+            
             for(int j = 0; j < n; j++) {
-                if(i == 0 && j == 0) dp[i][j] = 1;
-                else {
-                    int up = 0, left = 0;
+                
+                if(i == 0 && j == 0) {
+                    curr[j] = 1;
+                    continue;
+                } 
                     
-                    if(i > 0) up = dp[i - 1][j];
-                    if(j > 0) left = dp[i][j - 1];
-                    dp[i][j] = up + left;
-                }
+                int up = 0, left = 0;
+                if(i > 0) up = prev[j];
+                if(j > 0) left = curr[j - 1];
+                    
+                curr[j] = up + left;
             }
+            
+            prev = curr;
         }
         
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
 
+
+///// TABULATION //////
+
+// class Solution {
+// public:
+//     int uniquePaths(int m, int n) {
+        
+//         vector<vector<int>> dp(m,vector<int>(n));
+//         dp[0][0] = 1;
+        
+//         for(int i = 0; i < m; i++) {
+//             for(int j = 0; j < n; j++) {
+//                 if(i == 0 && j == 0) dp[i][j] = 1;
+//                 else {
+//                     int up = 0, left = 0;
+                    
+//                     if(i > 0) up = dp[i - 1][j];
+//                     if(j > 0) left = dp[i][j - 1];
+//                     dp[i][j] = up + left;
+//                 }
+//             }
+//         }
+        
+//         return dp[m-1][n-1];
+//     }
+// };
 
 ///// MEMOIZATION /////
 
