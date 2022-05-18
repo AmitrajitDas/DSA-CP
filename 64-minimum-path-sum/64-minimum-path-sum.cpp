d5da31f03d3dddf61abc7a2c0cb49188ebc53ac4
Amitrajit Das <amitrajitdas31@gmail.com>
2022-05-18 12:20:47 +0530

    Time: 15 ms (38.46%), Space: 10.1 MB (48.81%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -5,35 +5,78 @@ public:
     {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n));
+        vector<int> prev(n, 0);
         
         for(int i = 0; i < m; i++)
         {
+            vector<int> curr(n, 0);
+            
             for(int j = 0; j < n; j++) 
             {
                 if(i == 0 && j == 0)
                 {
-                    dp[i][j] = grid[0][0];
+                    curr[j] = grid[0][0];
                     continue;
                 }
                 
                 else {
                     int up = 0, left = 0;
                     
-                    if(i > 0) up = grid[i][j] + dp[i - 1][j];
+                    if(i > 0) up = grid[i][j] + prev[j];
                     else up = grid[i][j] + 1e9;
                     
-                    if(j > 0) left = grid[i][j] + dp[i][j - 1];
+                    if(j > 0) left = grid[i][j] + curr[j - 1];
                     else left = grid[i][j] + 1e9;
                     
-                    dp[i][j] = min(up, left);
+                    curr[j] = min(up, left);
                 }
             }
+            
+            prev = curr;
         }
         
-        return dp[m - 1][n - 1];
+        return prev[n - 1];
     }
 };
+
+////// TABULATION ///////
+
+// class Solution {
+// public:
+    
+//     int minPathSum(vector<vector<int>> &grid) 
+//     {
+//         int m = grid.size();
+//         int n = grid[0].size();
+//         vector<vector<int>> dp(m, vector<int>(n));
+        
+//         for(int i = 0; i < m; i++)
+//         {
+//             for(int j = 0; j < n; j++) 
+//             {
+//                 if(i == 0 && j == 0)
+//                 {
+//                     dp[i][j] = grid[0][0];
+//                     continue;
+//                 }
+                
+//                 else {
+//                     int up = 0, left = 0;
+                    
+//                     if(i > 0) up = grid[i][j] + dp[i - 1][j];
+//                     else up = grid[i][j] + 1e9;
+                    
+//                     if(j > 0) left = grid[i][j] + dp[i][j - 1];
+//                     else left = grid[i][j] + 1e9;
+                    
+//                     dp[i][j] = min(up, left);
+//                 }
+//             }
+//         }
+        
+//         return dp[m - 1][n - 1];
+//     }
+// };
 
 ////// MEMOIZATION //////
 
