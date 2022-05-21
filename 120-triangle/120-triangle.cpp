f79f38e32b86a33cb2403300994745ee63471e33
Amitrajit Das <amitrajitdas31@gmail.com>
2022-05-21 12:40:47 +0530

    Time: 3 ms (97.91%), Space: 9 MB (25.20%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,24 +1,50 @@
 class Solution {
 public:
-    
-    int recur(int i, int j, vector<vector<int>> &triangle, vector<vector<int>> &dp) 
-    {
-        
-        if(i == triangle.size() - 1) return triangle[i][j];
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int down = triangle[i][j] + recur(i + 1, j, triangle,dp);
-        int diag = triangle[i][j] + recur(i + 1, j + 1, triangle, dp);
-        
-        return dp[i][j] = min(down, diag);
-    }
-    
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int> (n, -1));
-        return recur(0, 0, triangle,dp);
+        vector<vector<int>> dp(n, vector<int> (n));
+        
+        for(int j = 0; j < n; j++)
+            dp[n - 1][j] = triangle[n - 1][j];
+        
+        for(int i = n - 2; i >= 0; i--) {
+            for(int j = i; j >= 0; j--) {
+                int down = 0, diag = 0;
+                
+                down = triangle[i][j] + dp[i + 1][j];
+                diag = triangle[i][j] + dp[i + 1][j + 1];
+                
+                dp[i][j] = min(down, diag);
+            }
+        }
+        
+        return dp[0][0];
     }
 };
+
+/////// MEMOIZATION //////
+
+// class Solution {
+// public:
+    
+//     int recur(int i, int j, vector<vector<int>> &triangle, vector<vector<int>> &dp) 
+//     {
+        
+//         if(i == triangle.size() - 1) return triangle[i][j];
+//         if(dp[i][j] != -1) return dp[i][j];
+        
+//         int down = triangle[i][j] + recur(i + 1, j, triangle,dp);
+//         int diag = triangle[i][j] + recur(i + 1, j + 1, triangle, dp);
+        
+//         return dp[i][j] = min(down, diag);
+//     }
+    
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         int n = triangle.size();
+//         vector<vector<int>> dp(n, vector<int> (n, -1));
+//         return recur(0, 0, triangle,dp);
+//     }
+// };
 
 /////// RECURSION ///////
 
