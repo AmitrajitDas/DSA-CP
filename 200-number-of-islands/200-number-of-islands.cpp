b2a89814362d6cebfc2ec302c906f451c3c26093
Amitrajit Das <amitrajitdas31@gmail.com>
2022-06-06 22:28:04 +0530

    Time: 94 ms (10.39%), Space: 18.2 MB (18.17%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,31 +1,73 @@
-class Solution {
+class Solution
+{
 public:
-    
-    void dfs(vector<vector<char>> &grid, int i, int j) {
-        
-        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == '0')
-            return;
-        
-        grid[i][j] = '0';
-        dfs(grid, i - 1, j); // Top
-        dfs(grid, i, j + 1); // Right
-        dfs(grid, i + 1, j); // Down
-        dfs(grid, i, j - 1); // Left
-    }
-    
-    int numIslands(vector<vector<char>>& grid) {
-        
-        int islands = 0;
-        
-        for(int i = 0; i < grid.size(); i++) {
-            for(int j = 0; j < grid[0].size(); j++) {
-                if(grid[i][j] == '1') {
+    int numIslands(vector<vector<char>> &grid)
+    {
+        int m = grid.size(), n = grid[0].size(), islands = 0;
+        int dir[] = {-1, 0, 1, 0, -1};
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (grid[i][j] == '1')
+                {
                     islands++;
-                    dfs(grid, i, j);
+                    grid[i][j] = '0';
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+                    while (!q.empty())
+                    {
+                        pair<int, int> p = q.front();
+                        q.pop();
+                        for (int k = 0; k < 4; k++)
+                        {
+                            int r = p.first + dir[k], c = p.second + dir[k + 1];
+                            if (r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == '1')
+                            {
+                                q.push({r, c});
+                                grid[r][c] = '0';
+                            }
+                        }
+                    }
                 }
             }
         }
-        
         return islands;
     }
 };
+
+//////// DFS SOLUTION ///////////
+
+// class Solution {
+// public:
+    
+//     void dfs(vector<vector<char>> &grid, int i, int j, int rows, int cols) {
+        
+//         if(i < 0 || i >= rows || j < 0 || j >= cols|| grid[i][j] == '0')
+//             return;
+        
+//         grid[i][j] = '0';
+//         dfs(grid, i - 1, j); // Top
+//         dfs(grid, i, j + 1); // Right
+//         dfs(grid, i + 1, j); // Down
+//         dfs(grid, i, j - 1); // Left
+//     }
+    
+//     int numIslands(vector<vector<char>>& grid) {
+        
+//         int m = grid.size(), n = grid[0].size(), 
+//         int islands = 0;
+        
+//         for(int i = 0; i < m; i++) {
+//             for(int j = 0; j < n; j++) {
+//                 if(grid[i][j] == '1') {
+//                     islands++;
+//                     dfs(grid, i, j, m, n);
+//                 }
+//             }
+//         }
+        
+//         return islands;
+//     }
+// };
