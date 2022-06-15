1a58584deb9ef85aaff667642ee855afaa61c08b
Amitrajit Das <amitrajitdas31@gmail.com>
2022-06-15 22:06:01 +0530

    Time: 14 ms (77.72%), Space: 11.6 MB (47.99%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,43 @@
+//// same as partitions with given difference ////
+
+class Solution
+{
+public:
+    int findTargetSumWays(vector<int> &arr, int d)
+    {
+        int n = arr.size();
+        int totSum = accumulate(arr.begin(), arr.end(), 0);
+        if ((totSum - d) < 0 || (totSum - d) % 2 != 0)
+            return false;
+        int tar = (totSum - d) / 2;
+        vector<int> prev(tar + 1, 0);
+
+        if (arr[0] <= 0)
+            prev[0] = 2;
+        else
+            prev[0] = 1;
+
+        if (arr[0] != 0 && arr[0] <= tar)
+            prev[arr[0]] = 1;
+
+        for (int idx = 1; idx < n; idx++)
+        {
+            vector<int> curr(tar + 1);
+            curr[0] = 1;
+            for (int sum = 0; sum <= tar; sum++)
+            {
+
+                int notTake = prev[sum];
+                int take = 0;
+                if (arr[idx] <= sum)
+                    take = prev[sum - arr[idx]];
+
+                curr[sum] = notTake + take;
+            }
+
+            prev = curr;
+        }
+
+        return prev[tar];
+    }
+};
