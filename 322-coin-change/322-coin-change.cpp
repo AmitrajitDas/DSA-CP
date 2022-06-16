c5ad1e44bcd4b714a524085c74b58239b3954725
Amitrajit Das <amitrajitdas31@gmail.com>
2022-06-17 00:28:50 +0530

    Time: 229 ms (14.10%), Space: 18.6 MB (26.72%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,29 +1,71 @@
-class Solution {
+////// TABULATION //////
+
+class Solution
+{
 public:
-    
-    int recurse(int idx, int target, vector<int> &coins, vector<vector<int>> &dp)
-    {
-        if(idx == 0)
-        {
-            if(target % coins[idx] == 0) return target / coins[idx];
-            else return 1e9;
-        }
-        
-        if(dp[idx][target] != -1) return dp[idx][target];
-        
-        int notTake = 0 + recurse(idx - 1, target, coins, dp);
-        int take = 1e9;
-        if(coins[idx] <= target)
-            take = 1 + recurse(idx, target - coins[idx], coins, dp);
-        
-        return dp[idx][target] = min(notTake, take);
-    }
-    
-    int coinChange(vector<int>& coins, int amount) 
+    int coinChange(vector<int> &coins, int amount)
     {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount + 1, -1));
-        int res = recurse(n - 1, amount, coins, dp);
-        return res == 1e9 ? -1 : res;
+
+        vector<int> prev(amount + 1, 0), cur(amount + 1, 0);
+
+        for (int i = 0; i <= amount; i++)
+        {
+            if (i % coins[0] == 0)
+                prev[i] = i / coins[0];
+            else
+                prev[i] = 1e9;
+        }
+
+        for (int ind = 1; ind < n; ind++)
+        {
+            for (int target = 0; target <= amount; target++)
+            {
+
+                int notTake = 0 + prev[target];
+                int take = 1e9;
+                if (coins[ind] <= target)
+                    take = 1 + cur[target - coins[ind]];
+
+                cur[target] = min(notTake, take);
+            }
+            prev = cur;
+        }
+
+        int ans = prev[amount];
+        
+        return ans >= 1e9 ? -1 : ans;
     }
 };
+
+////// MEMOIZATION ///////
+
+// class Solution {
+// public:
+    
+//     int recurse(int idx, int target, vector<int> &coins, vector<vector<int>> &dp)
+//     {
+//         if(idx == 0)
+//         {
+//             if(target % coins[idx] == 0) return target / coins[idx];
+//             else return 1e9;
+//         }
+        
+//         if(dp[idx][target] != -1) return dp[idx][target];
+        
+//         int notTake = 0 + recurse(idx - 1, target, coins, dp);
+//         int take = 1e9;
+//         if(coins[idx] <= target)
+//             take = 1 + recurse(idx, target - coins[idx], coins, dp);
+        
+//         return dp[idx][target] = min(notTake, take);
+//     }
+    
+//     int coinChange(vector<int>& coins, int amount) 
+//     {
+//         int n = coins.size();
+//         vector<vector<int>> dp(n, vector<int> (amount + 1, -1));
+//         int res = recurse(n - 1, amount, coins, dp);
+//         return res == 1e9 ? -1 : res;
+//     }
+// };
