395804eeb92a514c5d0c46545110559ddf134dc2
Amitrajit Das <amitrajitdas31@gmail.com>
2022-06-17 00:30:37 +0530

    Time: 158 ms (33.43%), Space: 18.6 MB (26.72%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,4 +1,4 @@
-////// TABULATION //////
+////// SPACE OPTIMIZATION //////
 
 class Solution
 {
@@ -37,6 +37,46 @@ public:
         return ans >= 1e9 ? -1 : ans;
     }
 };
+
+
+////// TABULATION ////////
+
+
+// class Solution
+// {
+// public:
+//     int coinChange(vector<int> &coins, int amount)
+//     {
+//         int n = coins.size();
+
+//         vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+
+//         for (int i = 0; i <= amount; i++)
+//         {
+//             if (i % coins[0] == 0)
+//                 dp[0][i] = i / coins[0];
+//             else
+//                 dp[0][i] = 1e9;
+//         }
+
+//         for (int ind = 1; ind < n; ind++)
+//         {
+//             for (int target = 0; target <= amount; target++)
+//             {
+
+//                 int notTake = 0 + dp[ind - 1][target];
+//                 int take = 1e9;
+//                 if (coins[ind] <= target)
+//                     take = 1 + dp[ind][target - coins[ind]];
+
+//                 dp[ind][target] = min(notTake, take);
+//             }
+//         }
+
+//         int ans = dp[n - 1][amount];
+//         return ans >= 1e9 ? -1 : ans;
+//     }
+// };
 
 ////// MEMOIZATION ///////
 
