7557a7b5810dabac7218d10cb69c9197dd8ad774
Amitrajit Das <amitrajitdas31@gmail.com>
2022-06-19 21:13:59 +0530

    Time: 56 ms (87.11%), Space: 26.4 MB (85.68%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -3,13 +3,14 @@ public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) 
     {
         vector<vector<string>> res;
-        sort(products.begin(), products.end());
+        sort(products.begin(), products.end()); // sort the list lexicographically
         int low = 0, high = products.size() - 1;
         
         for(int i = 0; i < searchWord.size(); i++)
         {
             char c = searchWord[i];
-            while(low <= high && (products[low].size() <= i || products[low][i] != c))
+            // pointer and string size and character macthing condistions
+            while(low <= high && (products[low].size() <= i || products[low][i] != c)) 
                 low++;
             while(low <= high && (products[high].size() <= i || products[high][i] != c))
                 high--;
@@ -17,7 +18,7 @@ public:
             vector<string> temp;
             int rem = high - low + 1;
             
-            for(int j = 0; j < min(rem, 3); j++)
+            for(int j = 0; j < min(rem, 3); j++) // we take the limit as min of 3 and remain window
             {
                 temp.push_back(products[low + j]);
             }
