412af96f81f0e20f41fc405c16601ddb2769e54e
Amitrajit Das <amitrajitdas31@gmail.com>
2022-06-21 21:23:03 +0530

    Time: 30 ms (58.75%), Space: 6.4 MB (96.26%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,22 +1,49 @@
+///// SPACE OPTIMIZATION /////
+
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size(), m = text2.size();
-        vector<vector<int>> dp(n + 1 , vector<int> (m + 1));
+        vector<int> prev(m + 1, 0), curr(m + 1, 0);
         
-        for(int i = 0; i <= n; i++) dp[i][0] = 0;
-        for(int j = 0; j <= m; j++) dp[0][j] = 0;
+        for(int i = 0; i <= m; i++) prev[i] = 0;
         
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= m; j++) {
-                if(text1[i - 1] == text2[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
-                else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                if(text1[i - 1] == text2[j - 1]) curr[j] = 1 + prev[j - 1];
+                else curr[j] = max(prev[j], curr[j - 1]);
             }
+            
+            prev = curr;
         }
         
-        return dp[n][m];
+        return prev[m];
     }
 };
+
+///// TABULATION /////
+
+// class Solution {
+// public:
+//     int longestCommonSubsequence(string text1, string text2) {
+//         int n = text1.size(), m = text2.size();
+//         vector<vector<int>> dp(n + 1 , vector<int> (m + 1));
+        
+//         for(int i = 0; i <= n; i++) dp[i][0] = 0;
+//         for(int j = 0; j <= m; j++) dp[0][j] = 0;
+        
+//         for(int i = 1; i <= n; i++) {
+//             for(int j = 1; j <= m; j++) {
+//                 if(text1[i - 1] == text2[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
+//                 else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+//             }
+//         }
+        
+//         return dp[n][m];
+//     }
+// };
+
+///// MEMOIZATION /////
 
 // class Solution {
 // public:
