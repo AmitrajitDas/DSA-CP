4cd81141a673705d80a2293ea8e04ec3e72a52fe
Amitrajit Das <amitrajitdas31@gmail.com>
2022-07-15 02:38:14 +0530

    Time: 171 ms (17.73%), Space: 11.2 MB (66.66%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -4,13 +4,18 @@ public:
         int n = s.size(), m = p.size();
         vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
         
-        // base case starts
-        
         dp[0][0] = true;
-        // for(int i = 1; i <= n; i++) dp[i][0] = false;
+        for(int i = 1; i <= n; i++) dp[i][0] = false;
         for(int j = 1; j <= m; j++) {
-            if(p[j - 1] != '*') break;
-            dp[0][j] = true;
+            bool flag = true;
+            for(int k = 1; k <= j; k++) {
+                if(p[k - 1] != '*') {
+                    flag = false; 
+                    break;
+                } 
+             }
+            
+             dp[0][j] = flag;
         }
         
         // base case ends
@@ -19,37 +24,16 @@ public:
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= m; j++) {
                 if(s[i - 1] == p[j - 1] || p[j - 1] == '?') dp[i][j] = dp[i - 1][j - 1];
-                else if(p[j - 1] == '*') dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-                else dp[i][j] = false;
+                else {
+                    if(p[j - 1] == '*') dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                    else dp[i][j] = false;
+                } 
             }
         }
         
         return dp[n][m];
     }
 };
-
-// class Solution {
-// public:
-//     bool f(int i,int j,string& s,string& p,vector<vector<int>>& dp) {
-//         if (i<0) {
-//             while (j>=0) {
-//                 if(p[j]!='*') return false;
-//                 j--;
-//             }
-//             return true;
-//         }
-//         if (j<0) return 0;
-//         if (dp[i][j]!=-1) return dp[i][j];
-        
-//         if (s[i]==p[j]||p[j]=='?') return dp[i][j]=f(i-1,j-1,s,p,dp);
-//         if (p[j]=='*') return dp[i][j]=f(i-1,j-1,s,p,dp)||f(i-1,j,s,p,dp)||f(i,j-1,s,p,dp);
-//         return 0;
-//     }
-//     bool isMatch(string s, string p) {
-//         vector<vector<int>> dp(s.size(),vector<int>(p.size(),-1));
-//         return f(s.size()-1,p.size()-1,s,p,dp);
-//     }
-// };
 
 // class Solution {
 // public:
