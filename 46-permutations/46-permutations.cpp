693cec20ed3fc3a1ea03e8725c38aa3996c09402
Amitrajit Das <amitrajitdas31@gmail.com>
2022-07-19 13:40:05 +0530

    Time: 9 ms (23.30%), Space: 7.6 MB (91.79%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,54 +1,29 @@
 class Solution {
 public:
     
-    void findPermute(vector<int> &nums, vector<vector<int>> &res, int idx) {
+    void backtrack(vector<int> &nums, vector<bool> &freq, vector<int> &arr, vector<vector<int>> &res) {
         
-        if(idx == nums.size()) {
-            res.push_back(nums);
+        if(arr.size() == nums.size()) {
+            res.push_back(arr);
             return;
         }
         
-        for(int i = idx; i < nums.size(); i++) {
-            swap(nums[idx], nums[i]);
-            findPermute(nums, res, idx + 1);
-            swap(nums[idx], nums[i]);
+        for(int i = 0; i < nums.size(); i++) {
+            if(!freq[i]) {
+                arr.push_back(nums[i]);
+                freq[i] = true;
+                backtrack(nums, freq, arr, res);
+                freq[i] = false;
+                arr.pop_back();
+            }
         }
     }
     
     vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> arr;
+        vector<bool> freq(nums.size(), false);
         vector<vector<int>> res;
-        findPermute(nums, res,0);
+        backtrack(nums, freq, arr, res);
         return res;
     }
 };
-
-// class Solution {
-// public:
-    
-//     void findPermute(vector<int> &nums, vector<int> &ds, vector<vector<int>> &res, vector<bool> &map) {
-        
-//         if(ds.size() == nums.size()) {
-//             res.push_back(ds);
-//             return;
-//         }
-        
-//         for(int i = 0; i < nums.size(); i++) {
-//             if(!map[i]) {
-//                 map[i] = true;
-//                 ds.push_back(nums[i]);
-//                 findPermute(nums, ds, res, map);
-//                 map[i] = false;
-//                 ds.pop_back();
-//             }
-//         }
-        
-//     }
-    
-//     vector<vector<int>> permute(vector<int>& nums) {
-//         vector<vector<int>> res;
-//         vector<int> ds;
-//         vector<bool> map(nums.size(), false);
-//         findPermute(nums, ds, res, map);
-//         return res;
-//     }
-// };
