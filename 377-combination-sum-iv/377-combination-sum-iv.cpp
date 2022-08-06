4fa85a58db66348fdc894aef158e760008d87209
Amitrajit Das <amitrajitdas31@gmail.com>
2022-08-06 20:21:52 +0530

    Time: 5 ms (33.70%), Space: 6.7 MB (30.10%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
-    
-    int memoize(vector<int>& nums, int target, vector<int>& dp){
-        if(target == 0) return 1;
-        if(dp[target] != -1) return dp[target];
-        dp[target] = 0;
-        for(auto& num : nums) 
-            if(num <= target) dp[target] += memoize(nums, target - num, dp);
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<uint> dp(target + 1); dp[0] = 1;
+        for (int curTarget = 1; curTarget <= target; curTarget++) 
+            for (auto& num : nums)
+                if (num <= curTarget) dp[curTarget] += dp[curTarget - num];   
         return dp[target];
     }
-    
-    int combinationSum4(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<int> dp(target + 1, -1);
-        return memoize(nums, target, dp);
-    }
 };
+
+
+// class Solution {
+// public:
+    
+//     int memoize(vector<int>& nums, int target, vector<int>& dp){
+//         if(target == 0) return 1;
+//         if(dp[target] != -1) return dp[target];
+//         dp[target] = 0;
+//         for(auto& num : nums) 
+//             if(num <= target) dp[target] += memoize(nums, target - num, dp);
+//         return dp[target];
+//     }
+    
+//     int combinationSum4(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         vector<int> dp(target + 1, -1);
+//         return memoize(nums, target, dp);
+//     }
+// };
