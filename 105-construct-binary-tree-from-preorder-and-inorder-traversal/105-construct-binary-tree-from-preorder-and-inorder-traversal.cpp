14fdf609d48749b2e134db7a9a6ac61729ca8182
Amitrajit Das <amitrajitdas31@gmail.com>
2022-08-17 22:34:45 +0530

    Update 105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -12,26 +12,25 @@
 class Solution {
 public:
     
-    TreeNode* solve(vector<int>& preorder, int preStart, int preEnd, vector<int>& inorder, int inStart,                           int inEnd, unordered_map<int, int> &map) {
-        
+    TreeNode* buildTree(map<int, int> &inMap, vector<int>& preorder, int preStart, int preEnd, vector<int>& inorder, int inStart, int inEnd) {
         if(preStart > preEnd || inStart > inEnd) return NULL;
         
         TreeNode* root = new TreeNode(preorder[preStart]);
-        int inRoot = map[root->val];
+        int inRoot = inMap[root->val];
         int numsLeft = inRoot - inStart;
         
-        root->left = solve(preorder, preStart + 1, preStart + numsLeft, inorder, inStart, inRoot - 1, map);
-        root->right = solve(preorder, preStart + numsLeft + 1, preEnd, inorder, inRoot + 1, inEnd, map);
+        root->left = buildTree(inMap, preorder, preStart + 1, preStart + numsLeft, inorder, inStart, inRoot - 1);
+        root->right = buildTree(inMap, preorder, preStart + numsLeft + 1, preEnd, inorder, inRoot + 1, inEnd);
         
         return root;
     }
     
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        unordered_map<int, int> map;
+        map<int, int> inMap;
         
-        for(int i = 0; i < inorder.size(); i++) map[inorder[i]] = i;
+        for(int i = 0; i < inorder.size(); i++) inMap[inorder[i]] = i; // hashing inorder array
         
-        TreeNode* root = solve(preorder, 0, preorder.size() - 1, inorder, 0 , inorder.size() - 1, map);
+        TreeNode* root = buildTree(inMap, preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1);
         
         return root;
     }
