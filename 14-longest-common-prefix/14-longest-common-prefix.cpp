3081ff292181bf119fdd87a9abfae31526ed2378
Amitrajit Das <amitrajitdas31@gmail.com>
2022-08-21 17:46:31 +0530

    Update 14-longest-common-prefix.cpp

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,34 +1,15 @@
-class Solution
-{
+class Solution {
 public:
-    string longestCommonPrefix(vector<string> &strs)
-    {
-
-        string prefix = strs[0];
+    string longestCommonPrefix(vector<string>& strs) {
         int n = strs.size();
-        if(n == 0) return "";
-
-        for (int i = 1; i < n; i++)
-        {
-            if (strs[i] == prefix) // if any string matches with prefix then we don't evaluate
-                continue;
-            else
-            {
-
-                string temp = "";
-                for (int j = 0; j < strs[i].length() && j < prefix.length(); j++)
-                {
-                    // we check how much of the curr string is matching with prefix
-                    if (strs[i][j] == prefix[j])
-                        temp += strs[i][j];
-                    else
-                        break;
-                }
-
-                prefix = temp; // updated prefix
-            }
+        sort(strs.begin(), strs.end());
+        
+        string start = strs[0], end = strs[n - 1]; // start and end strings will be the least matched so if we compare them only we can the LCP
+        int i = 0;
+        while(i < start.length() && start[i] == end[i]) {
+            i++;
         }
         
-        return prefix;
+        return i == 0 ? "" : start.substr(0, i);
     }
 };
