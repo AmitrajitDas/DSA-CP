fcf71e925ff12dace1bf6a3d8c68644a46778d4a
Amitrajit Das <amitrajitdas31@gmail.com>
2022-08-26 00:12:29 +0530

    Update 876-middle-of-the-linked-list.cpp

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -11,12 +11,10 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        
-        ListNode* slow = head;
-        ListNode* fast = head;
+        if(!head) return head;
+        ListNode *slow = head, *fast = head;
         
         while(fast && fast->next) {
-            
             slow = slow->next;
             fast = fast->next->next;
         }
