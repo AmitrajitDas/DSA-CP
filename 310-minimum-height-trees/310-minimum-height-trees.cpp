edfeb15b7a59d76c9aab515e5cdc1bc3cf08f44b
Amitrajit Das <amitrajitdas31@gmail.com>
2022-09-16 01:33:22 +0530

    Time: 347 ms (28.65%), Space: 49.9 MB (94.58%) - LeetHub

---
+++ 310-minimum-height-trees/310-minimum-height-trees.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        if(n==0)
+            return {};
+        if(n==1)
+            return {0};
+        
+        vector<int> degree(n, 0);
+        vector<int> adj[n];
+        
+        // bulding adjacency list and degree
+        for(int i = 0; i < edges.size(); i++) {
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+            degree[edges[i][0]]++;
+            degree[edges[i][1]]++;
+        }
+        
+        queue<int> q;
+        for(int i = 0; i < n; i++) {
+            if(degree[i] == 1) q.push(i);
+        }
+        
+        vector<int> topo;
+        while(!q.empty()) {
+            topo.clear();
+            int size = q.size();
+            for(int i = 0; i < size; i++) {
+                int node = q.front();
+                q.pop();
+                topo.push_back(node);
+            
+                for(auto it : adj[node]) {
+                    if(--degree[it] == 1) q.push(it);
+                }
+            }
+        }
+        
+        return topo;
+    }
+};
