3be67845aeffba1dfcf262e5dd109de9eba97f1b
Amitrajit Das <amitrajitdas31@gmail.com>
2022-10-10 22:45:27 +0530

    Time: 5 ms (20.18%), Space: 6.3 MB (45.53%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        int n = palindrome.size();
+        
+        for(int i = 0; i < n/2; i++) {
+            if(palindrome[i] != 'a') {
+                palindrome[i] = 'a';
+                return palindrome;
+            }
+        }
+        
+        palindrome[n - 1] = 'b';
+        return  n > 1 ? palindrome : "";
+    }
+};
