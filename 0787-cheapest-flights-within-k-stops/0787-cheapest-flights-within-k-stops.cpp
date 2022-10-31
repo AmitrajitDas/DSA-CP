9de15c67faa0097cf129bda27cd5633b7b34426d
Amitrajit Das <amitrajitdas31@gmail.com>
2022-10-31 22:53:51 +0530

    Time: 46 ms (71.25%), Space: 13.7 MB (46.83%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -14,17 +14,22 @@ public:
             int stops = it.first;
             int node = it.second.first, cost = it.second.second;
             
+            // if we reach destination with k+1 move then stop 
+            // iteration as we reached destination with atmost 
+            // k stops 
             if(stops > k) break;
             
             for(auto itr : adj[node]) {
                 int adjNode = itr.first, edgeWt = itr.second;
                 if(cost + edgeWt < dist[adjNode] && stops <= k) {
-                    dist[adjNode] = cost + edgeWt;
-                    q.push({stops + 1, {adjNode, dist[adjNode]}});
+                    dist[adjNode] = cost + edgeWt; // update the distance 
+                    q.push({stops + 1, {adjNode, dist[adjNode]}});  // push the adj element and new distance into queue
                 }
             }
         }
         
+        // if the distance of the dst is not changed the return -1
+        // as it is not possible to reach destination
         return dist[dst] == 1e9 ? -1 : dist[dst];
     }
 };
