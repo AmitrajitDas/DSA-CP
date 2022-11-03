6759517693cd7a3ea4091205559f94b894224b79
Amitrajit Das <amitrajitdas31@gmail.com>
2022-11-03 20:28:48 +0530

    Time: 790 ms (52.85%), Space: 167.9 MB (60.00%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
-    unordered_map<string, int> m;
-    int unpaired = 0, ans = 0;
-    for (string w: words) {
-        if (w[0] == w[1]) {
-            if (m[w] > 0) {
-                unpaired--;
-                m[w]--;
-                ans += 4;
+        unordered_map<string, int> m;
+        int unpaired = 0, ans = 0;
+        for (string w: words) {
+            if (w[0] == w[1]) {
+                if (m[w] > 0) {
+                    unpaired--;
+                    m[w]--;
+                    ans += 4;
+                }
+                else {
+                    m[w]++;
+                    unpaired++;
+                }
             }
             else {
-                m[w]++;
-                unpaired++;
+                string rev = w;
+                reverse(rev.begin(), rev.end());
+                if (m[rev] > 0) {
+                    ans += 4;
+                    m[rev]--;
+                }
+                else m[w]++;
             }
         }
-        else {
-            string rev = w;
-            reverse(rev.begin(), rev.end());
-            if (m[rev] > 0) {
-                ans += 4;
-                m[rev]--;
-            }
-            else m[w]++;
-        }
+        return unpaired > 0 ? ans += 2 : ans;
     }
-    if (unpaired > 0) ans += 2;
-    return ans;
-}
 };
 
