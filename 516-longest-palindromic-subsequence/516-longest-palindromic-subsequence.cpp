6c787718b9f90229f9b06cc4a4c4b1615ddd631b
Amitrajit Das <amitrajitdas31@gmail.com>
2022-12-14 22:18:14 +0530

    Update 516-longest-palindromic-subsequence.cpp

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -19,3 +19,25 @@ public:
         return prev[n];
     }
 };
+
+// class Solution {
+// public:
+//     int longestPalindromeSubseq(string s1) {
+//         int n = s1.size();
+//         string s2 = string(s1.rbegin(), s1.rend());
+        
+//         vector<vector<int>> dp(n + 1, vector<int> (n + 1, 0));
+
+//         for(int j = 0; j <= n; j++) dp[0][j] = 0;
+//         for(int i = 0; i <= n; i++) dp[i][0] = 0;
+
+//         for(int i = 1; i <= n; i++) {
+//             for(int j = 1; j <= n; j++) {
+//                 if(s1[i - 1] == s2[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
+//                 else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+//             }
+//         }
+
+//         return dp[n][n];
+//     }
+// };
