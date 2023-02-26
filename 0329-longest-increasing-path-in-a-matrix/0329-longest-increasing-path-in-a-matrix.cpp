05b6d4b69589099e851cac29a7e2cd846566d50a
Amitrajit Das <amitrajitdas31@gmail.com>
2023-02-26 14:22:07 +0530

    Time: 58 ms (45.95%), Space: 16.7 MB (34.10%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -3,32 +3,24 @@ private:
     int dfs(int row, int col, int prev, vector<vector<int>> &matrix, vector<vector<int>> &dp) {
         if(row < 0 || row == matrix.size() || col < 0 || col == matrix[row].size() || matrix[row][col] <= prev) return 0;
         if(dp[row][col] != -1) return dp[row][col];
-
-        int res = 1;
-        res = max(1 + dfs(row - 1, col, matrix[row][col], matrix, dp), res);
-        res = max(1 + dfs(row, col + 1, matrix[row][col], matrix, dp), res);
-        res = max(1 + dfs(row, col - 1, matrix[row][col], matrix, dp), res);
-        res = max(1 + dfs(row + 1, col, matrix[row][col], matrix, dp), res);
-        dp[row][col] = res;
-        return res;
+        
+        return dp[row][col] = 1 + max({
+            dfs(row - 1, col, matrix[row][col], matrix, dp),
+            dfs(row, col + 1, matrix[row][col], matrix, dp),
+            dfs(row, col - 1, matrix[row][col], matrix, dp),
+            dfs(row + 1, col, matrix[row][col], matrix, dp)
+        });
     }
 public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int rows = matrix.size(), cols = matrix[0].size();
+        int lip;
         vector<vector<int>> dp(rows, vector<int> (cols, -1));
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < cols; j++) {
-                dfs(i, j, -1, matrix, dp);
+               lip = max(dfs(i, j, -1, matrix, dp), lip);
             }
         }
-
-        int lip;
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
-               lip = max(dp[i][j], lip);
-            }
-        }
-
         return lip;
     }
 };
