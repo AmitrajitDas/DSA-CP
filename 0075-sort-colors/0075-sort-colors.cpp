a46114ef6c069dbafa0cd92d96fa72a316228145
Amitrajit Das <amitrajitdas31@gmail.com>
2023-05-05 10:34:38 +0530

    Time: 6 ms (23.50%), Space: 8.2 MB (91.46%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,16 @@
+// TC: O(N), SC: O(1)
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int low = 0, mid = 0, high = n - 1;
+        // Dutch National Flag algo
+        while(mid <= high) {
+            if(nums[mid] == 0) swap(nums[low++], nums[mid++]);
+            else if(nums[mid] == 1) mid++;
+            else swap(nums[mid], nums[high--]);
+        }
+        return;
+    }
+};
