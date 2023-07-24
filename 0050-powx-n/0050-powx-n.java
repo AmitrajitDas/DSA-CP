f11ea55bdd7c85bb6aae212ee45c5524e2549bd8
Amitrajit Das <amitrajitdas31@gmail.com>
2023-07-25 00:55:43 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (20.14%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,35 @@
+// Binary Exponentiation is an efficient algorithm for calculating
+// the power of a number 'x' raised to the power 'n'.
+class Solution {
+    // Helper function to perform binary exponentiation recursively.
+    // The function takes the base 'x' and the exponent 'n'.
+    private double solve(double x, long n) {
+        // Base case: if the exponent is 0, return 1 as any number
+        // raised to the power of 0 is 1.
+        if (n == 0) return 1;
+
+        // If the exponent is negative, we use the property:
+        // x^n = 1 / (x^-n). So, we call the solve function with 1/x
+        // as the base and -n as the exponent.
+        if (n < 0) return solve(1 / x, -n);
+
+        // If the exponent is even, we use the property:
+        // x^n = (x^2)^(n/2). So, we call the solve function with
+        // x*x as the base and n/2 as the exponent.
+        if (n % 2 == 0) return solve(x * x, n / 2);
+
+        // If the exponent is odd, we use the property:
+        // x^n = x * (x^2)^(n/2). So, we call the solve function with
+        // x*x as the base and n/2 as the exponent, and multiply the
+        // result with 'x'.
+        return x * solve(x * x, n / 2);
+    }
+
+    // Main function to calculate x raised to the power of n.
+    public double myPow(double x, int n) {
+        // We convert the exponent 'n' to a long type to handle
+        // cases where 'n' is Integer.MIN_VALUE, which would cause
+        // overflow if not converted to long.
+        return solve(x, (long) n);
+    }
+}
