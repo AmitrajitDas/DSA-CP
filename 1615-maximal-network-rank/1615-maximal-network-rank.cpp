479d8d779de35a9046bcc4a19d7831d1206af70a
Amitrajit Das <amitrajitdas31@gmail.com>
2023-08-18 12:39:36 +0530

    Time: 61 ms (92.58%), Space: 28.3 MB (95.43%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -1,41 +1,3 @@
-//Approach-1 (Simply checking each pair of nodes) - O(V^2)
-// class Solution {
-// public:
-//     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-//         unordered_map<int, unordered_set<int>> adj;
-        
-//         for(auto &road : roads) {
-//             int u = road[0];
-//             int v = road[1];
-            
-//             adj[u].insert(v);
-//             adj[v].insert(u);
-//         }
-        
-//         int maxRank = 0;
-        
-//         for(int i = 0; i < n; i++) {
-//             for(int j = i+1; j < n; j++) {
-                
-//                 int i_rank = adj[i].size();
-//                 int j_rank = adj[j].size();
-                
-//                 int rank   = i_rank + j_rank;
-                
-//                 if(adj[i].find(j) != adj[i].end()) {
-//                     rank -= 1;
-//                 }
-                
-//                 maxRank = max(maxRank, rank);
-                
-//             }
-//         }
-        
-//         return maxRank;
-//     }
-// };
-
-//Approach-2 (Using degree count & keeping a check if nodes are connected) - O(V^2)
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
@@ -46,27 +8,27 @@ public:
             int u = road[0];
             int v = road[1];
             
+            // Increment the degrees of nodes u and v
             degree[u]++;
             degree[v]++;
             
+            // Mark the nodes as connected in the matrix
             connected[u][v] = true;
             connected[v][u] = true;
         }
         
         int maxRank = 0;
-        
+
         for(int i = 0; i < n; i++) {
             for(int j = i+1; j < n; j++) {
                 
                 int i_rank = degree[i];
                 int j_rank = degree[j];
                 
-                int rank   = i_rank + j_rank;
+                int rank = i_rank + j_rank;
                 
-                if(connected[i][j] || connected[j][i]) {
-                    rank -= 1;
-                }
-                
+                // Decrement the rank if there's a road between nodes i and j, or j and i
+                if(connected[i][j] || connected[j][i]) rank--;
                 maxRank = max(maxRank, rank);
                 
             }
