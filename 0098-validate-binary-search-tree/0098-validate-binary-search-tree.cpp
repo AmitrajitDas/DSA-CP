41b528a32af42f806b56c2ba1613ed05654b1dfb
Amitrajit Das <amitrajitdas31@gmail.com>
2023-12-10 00:08:58 +0530

    Time: 4 ms (91.56%), Space: 21.9 MB (99.15%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,14 +11,26 @@
  */
 class Solution {
 private:
-    bool dfs(TreeNode *root, long mini, long maxi) {
-        if(!root) return true;
+    bool dfs(TreeNode* root, long mini, long maxi) {
+        // If the current node is null, it is a valid subtree
+        if (!root) {
+            return true;
+        }
 
-        if(root->val <= mini || root->val >= maxi) return false;
+        // Check if the current node's value is within the valid range (mini, maxi)
+        if (root->val <= mini || root->val >= maxi) {
+            return false;
+        }
+
+        // Recursively check the left and right subtrees
+        // Update the range for the left subtree to (mini, root->val)
+        // Update the range for the right subtree to (root->val, maxi)
         return dfs(root->left, mini, root->val) && dfs(root->right, root->val, maxi);
     }
+
 public:
     bool isValidBST(TreeNode* root) {
+        // Start the DFS with an initial valid range for values in the tree
         return dfs(root, LONG_MIN, LONG_MAX);
     }
 };
