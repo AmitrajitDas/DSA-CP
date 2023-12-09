170469de0444ed87caebf229b9daadc3b035b24a
Amitrajit Das <amitrajitdas31@gmail.com>
2023-12-09 22:34:22 +0530

    Time: 0 ms (100.00%), Space: 11.5 MB (34.67%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
+    // Function to perform a rotated sorted array search (modified binary search)
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        if(n == 0) return -1;
-        int low = 0, high = n - 1;
+        int low = 0, high = nums.size() - 1;
 
-        while(low <= high) {
+        while (low <= high) {
             int mid = low + (high - low) / 2;
-            if(nums[mid] == target) return mid;
-            else if(nums[low] <= nums[mid]) {
-                if(target >= nums[low] && target < nums[mid]) high = mid - 1;
-                else low = mid + 1;
-            }
-            else if(nums[mid] <= nums[high]) {
-                if(target > nums[mid] && target <= nums[high]) low = mid + 1;
-                else high = mid - 1;
+            // Check if the middle element is equal to the target
+            if (nums[mid] == target) {
+                // If found, return the index
+                return mid;
+            } else if (nums[low] <= nums[mid]) {
+                // If the left half is sorted
+                // Check if the target is in the left half
+                if (target >= nums[low] && target < nums[mid]) {
+                    // Adjust the search space to the left half
+                    high = mid - 1;
+                } else {
+                    // Adjust the search space to the right half
+                    low = mid + 1;
+                }
+            } else if (nums[mid] <= nums[high]) {
+                // If the right half is sorted
+                // Check if the target is in the right half
+                if (target > nums[mid] && target <= nums[high]) {
+                    // Adjust the search space to the right half
+                    low = mid + 1;
+                } else {
+                    // Adjust the search space to the left half
+                    high = mid - 1;
+                }
             }
         }
 
+        // If the target is not found, return -1
         return -1;
     }
 };
