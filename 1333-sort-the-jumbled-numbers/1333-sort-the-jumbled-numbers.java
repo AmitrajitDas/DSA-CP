06d3cb6744489bde7a4ebb8a6d16cdcf9a588e58
Amitrajit Das <amitrajitdas31@gmail.com>
2024-07-25 00:56:26 +0530

    Time: 287 ms (40.2%), Space: 55.3 MB (87.25%) - LeetHub

--- 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.java
+++ 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.java
@@ -1,36 +1,45 @@
 class Solution {
+    // Method to translate a number based on the given mapping
     private int translateInteger(int num, int[] mapping) {
         if(num == 0) {
-            return mapping[0];
+            return mapping[0]; // Special case when num is 0
         }
         int res = 0, mult = 1;
 
+        // Translate the number by mapping each digit
         while(num > 0) {
-            int rem = num % 10;
-            num /= 10;
-            res += mapping[rem] * mult;
-            mult *= 10;
+            int rem = num % 10; // Get the last digit
+            num /= 10; // Remove the last digit from num
+            res += mapping[rem] * mult; // Map the digit and add to the result
+            mult *= 10; // Increase the multiplier by 10 for the next digit
         }
 
         return res;
     }
 
+    // Method to sort the array based on the translated values
     public int[] sortJumbled(int[] mapping, int[] nums) {
+        // Map to store the translated values for each number in nums
         Map<Integer, Integer> mp = new HashMap<>();
 
+        // Translate each number and store it in the map
         for(int num : nums) {
             if(!mp.containsKey(num)) {
-                mp.put(num, translateInteger(num, mapping));
+                mp.put(num, translateInteger(num, mapping)); // Only translate if not already done
             }
         }
 
+        // Convert nums to Integer array for sorting
         Integer[] numsBoxed = Arrays.stream(nums).boxed().toArray(Integer[]::new);
+
+        // Sort nums based on their translated values
         Arrays.sort(numsBoxed, (a, b) -> mp.get(a) - mp.get(b));
 
+        // Convert sorted Integer array back to int array
         for(int i = 0; i < nums.length; i++) {
             nums[i] = numsBoxed[i];
         }
 
-        return nums;
+        return nums; // Return the sorted array
     }
-}
+};
