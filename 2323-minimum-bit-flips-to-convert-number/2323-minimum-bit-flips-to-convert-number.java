33efa658263384bd79d21a6665a7db7148fccad6
Amitrajit Das <amitrajitdas31@gmail.com>
2024-09-11 16:29:10 +0530

    Time: 0 ms (100%), Space: 40.6 MB (35%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
@@ -1,13 +1,23 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        int count = 0;
+        int count = 0;  // Initialize a counter to track the number of bit flips needed
+        
+        // XOR the start and goal numbers
+        // XOR will give a 1 in each position where the bits of 'start' and 'goal' differ
         int xor = start ^ goal;
 
-        while(xor != 0) {
+        // Loop through all bits of the XOR result
+        while (xor != 0) {
+            // Check the least significant bit (LSB)
+            // `xor & 1` checks if the current bit is 1 (i.e., different between 'start' and 'goal')
             count += xor & 1;
+
+            // Right shift xor by 1 to check the next bit
+            // This moves the bits of 'xor' one position to the right
             xor >>= 1;
         }
-        
+
+        // Return the total number of different bits, which corresponds to the minimum number of bit flips
         return count;
     }
 }
