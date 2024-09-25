18cfbccfb8fd88157b77211f8119a38d6fa0c622
Amitrajit Das <amitrajitdas31@gmail.com>
2024-09-25 17:01:09 +0530

    Time: 5 ms (99.88%), Space: 45.9 MB (44.02%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,16 +1,32 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int n = matrix.length, m = matrix[0].length;
-        int row = 0, col = m - 1;
-        while(row < n && col >= 0) {
-            if(matrix[row][col] == target) {
+        // Step 1: Get the dimensions of the matrix
+        int n = matrix.length;       // Number of rows in the matrix
+        int m = matrix[0].length;    // Number of columns in the matrix
+
+        // Step 2: Start from the top-right corner of the matrix
+        int row = 0;     // Start from the first row
+        int col = m - 1; // Start from the last column (rightmost element of the first row)
+
+        // Step 3: Traverse the matrix using the properties of the sorted matrix
+        while (row < n && col >= 0) {
+            // Step 4: If the current element is equal to the target, return true
+            if (matrix[row][col] == target) {
                 return true;
-            } else if(matrix[row][col] < target) {
+            }
+            // Step 5: If the current element is less than the target, move down (increase row)
+            // This is because all elements below are larger, so we move to the next row
+            else if (matrix[row][col] < target) {
                 row++;
-            } else {
+            }
+            // Step 6: If the current element is greater than the target, move left (decrease column)
+            // This is because all elements to the left are smaller, so we move left
+            else {
                 col--;
             }
         }
+
+        // Step 7: If the loop exits, the target was not found, return false
         return false;
     }
 }
