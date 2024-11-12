0ab35cda508943a971bf356b575567f581f8d276
Amitrajit Das <amitrajitdas31@gmail.com>
2024-11-12 16:52:05 +0530

    Time: 1 ms (100%), Space: 45.7 MB (32.71%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,19 +1,30 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
         int xor = 0, rightmostSetbit = 0;
-        for(final int num : nums) xor ^= num;
 
+        // Perform XOR of all elements in the array. 
+        // This results in XOR of the two unique elements (all other elements cancel out).
+        for(final int num : nums) {
+            xor ^= num;
+        }
+
+        // Isolate the rightmost set bit in the XOR result.
+        // This bit is different between the two unique elements.
         rightmostSetbit = (xor & (xor - 1)) ^ xor;
+
         int b1 = 0, b2 = 0;
 
+        // Divide the numbers into two groups based on the isolated bit
+        // and XOR each group separately to get the two unique elements.
         for(final int num : nums) {
             if((num & rightmostSetbit) != 0) {
-                b1 ^= num;
+                b1 ^= num; // XOR of first group
             } else {
-                b2 ^= num;
+                b2 ^= num; // XOR of second group
             }
         }
 
+        // Return the two unique numbers found
         return new int[]{b1, b2};
     }
 }
