b6de8c3634783128e19bebe341ea18ddb9398893
Amitrajit Das <amitrajitdas31@gmail.com>
2025-06-25 23:15:43 +0530

    Time: 0 ms (100%), Space: 40.6 MB (37.89%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,12 +1,15 @@
 class Solution {
-    int[] dp = new int[20];
-    // Arrays.fill(dp, -1);
     public int numTrees(int n) {
-        if(n <= 1) return 1;
-        if(dp[n] != 0) return dp[n];
-        for(int i = 1; i <= n; i++) {
-            dp[n] += numTrees(i - 1) * numTrees(n - i);
+        int[] dp = new int[20];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
         }
+
         return dp[n];
     }
 }
