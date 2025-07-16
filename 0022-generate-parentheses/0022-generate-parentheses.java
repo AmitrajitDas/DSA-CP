33bb154a28c32e3f68b0d4d2f2cb701de8339d31
Amitrajit Das <amitrajitdas31@gmail.com>
2025-07-16 21:37:11 +0530

    Time: 8 ms (6.17%), Space: 43.4 MB (47.56%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,20 +1,4 @@
 class Solution {
-    private void generate(int opened, int closed, int n, StringBuilder sb, List<String> res) {
-        if(opened == n && closed == n) {
-            res.add(sb.toString());
-        }
-
-        if(opened < n) {
-            sb.append('(');
-            generate(opened + 1, closed, n, sb, res);
-            sb.deleteCharAt(sb.length() - 1); // backtrack
-        }
-        if(closed < opened) {
-            sb.append(')');
-            generate(opened, closed + 1, n, sb, res);
-            sb.deleteCharAt(sb.length() - 1); // backtrack
-        } 
-    }
 
     public List<String> generateParenthesis(int n) {
         // dp[i] stores all valid combinations for i pairs of parentheses
