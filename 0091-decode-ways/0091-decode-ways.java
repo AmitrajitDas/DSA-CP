21c69efb71db490eef96805a3d5a87da13d00fcf
Amitrajit Das <amitrajitdas31@gmail.com>
2025-07-24 20:34:33 +0530

    Time: 0 ms (100%), Space: 41.7 MB (87.2%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,26 +1,24 @@
-class Solution {
-    private int memo(int idx, int n, String s, int[] dp) {
-        if(idx == n) return 1;  // Base case: reached end successfully
-        if(s.charAt(idx) == '0') return 0; // Invalid: can't start with 0
-        if(dp[idx] != -1) return dp[idx];
-        
-        // Single digit decoding
-        int res = memo(idx + 1, n, s, dp);
-        
-        // Two digit decoding (10-26)
-        if(idx < n - 1 && (s.charAt(idx) == '1' || 
-           (s.charAt(idx) == '2' && s.charAt(idx + 1) < '7'))) {
-            res += memo(idx + 2, n, s, dp);
-        }
-        
-        return dp[idx] = res;
-    }
-    
+class Solution {    
     public int numDecodings(String s) {
         int n = s.length();
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        
-        return memo(0, n, s, dp);
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
+
+        for(int i = n - 1; i >= 0; i--) {
+            if(s.charAt(i) == '0') {
+                dp[i] = 0;
+                continue;
+            }
+            
+            int res = dp[i + 1];
+            if(i < n - 1 && (s.charAt(i) == '1' || 
+            (s.charAt(i) == '2' && s.charAt(i + 1) < '7'))) {
+                res += dp[i + 2];
+            }
+
+            dp[i] = res;
+        }
+
+        return dp[0];
     }
 }
