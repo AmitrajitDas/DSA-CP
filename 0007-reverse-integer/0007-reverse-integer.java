cb4488be4d86e1f4016bb91c4b34ce1c587daba7
Amitrajit Das <amitrajitdas31@gmail.com>
2025-08-27 20:13:50 +0530

    Time: 1 ms (81.56%), Space: 40.8 MB (71.73%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,14 +1,31 @@
 class Solution {
+    /**
+     * Reverses the digits of a 32-bit signed integer.
+     * Returns 0 if the reversed integer overflows outside the range [-2^31, 2^31 - 1].
+     * 
+     * @param x the integer to reverse
+     * @return the reversed integer, or 0 if overflow occurs
+     */
     public int reverse(int x) {
-        int rev = 0;
+        int rev = 0; // Initialize reversed number to 0
+        
+        // Continue processing while there are digits left in x
         while(x != 0) {
+            // Check for overflow before multiplying by 10
+            // If rev > MAX_VALUE/10, then rev*10 would exceed MAX_VALUE
+            // If rev < MIN_VALUE/10, then rev*10 would be less than MIN_VALUE
             if(rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10) {
-                return 0;
+                return 0; // Return 0 if overflow would occur
             }
+            
+            // Extract the last digit of x using modulo operator
+            // Add it to rev after shifting rev one decimal place left
             rev = rev * 10 + x % 10;
+            
+            // Remove the last digit from x by integer division
             x /= 10;
         }
-
-        return rev;
+        
+        return rev; // Return the reversed number
     }
 }
