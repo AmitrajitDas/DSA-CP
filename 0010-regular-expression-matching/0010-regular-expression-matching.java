18354ec4e5b0b098e3cd96a3fb560108ee005be1
Amitrajit Das <amitrajitdas31@gmail.com>
2025-10-16 00:20:18 +0530

    Time: 2 ms (60.41%), Space: 42 MB (88.56%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,22 +1,41 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int n = s.length(), m = p.length();
-        boolean[][] dp = new boolean[n + 1][m + 1];
-
-        dp[0][0] = true;
-
-        for (int i = 0; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (p.charAt(j - 1) == '*') {
-                    dp[i][j] = dp[i][j - 2] || (i > 0 && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.')
-                            && dp[i - 1][j]);
-                } else {
-                    dp[i][j] = i > 0 && dp[i - 1][j - 1]
-                            && (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.');
-                }
-            }
+        return helper(s, p, s.length() - 1, p.length() - 1);
+    }
+    
+    private boolean helper(String s, String p, int i, int j) {
+        // Base case: if pattern is exhausted
+        if (j < 0) {
+            return i < 0;
         }
-
-        return dp[n][m];
+        
+        // Base case: if string is exhausted but pattern remains
+        if (i < 0) {
+            // Pattern can only match if remaining pattern is like "a*b*c*"
+            // Check if current and previous form a '*' pattern
+            if (j > 0 && p.charAt(j) == '*') {
+                return helper(s, p, i, j - 2);
+            }
+            return false;
+        }
+        
+        // Check if current characters match
+        boolean currentMatch = (p.charAt(j) == s.charAt(i) || p.charAt(j) == '.');
+        
+        // Check if current character in pattern is '*'
+        if (p.charAt(j) == '*') {
+            // '*' must have a preceding character
+            char prevChar = p.charAt(j - 1);
+            boolean prevMatch = (prevChar == s.charAt(i) || prevChar == '.');
+            
+            // Two choices:
+            // 1. Don't use the '*' (match zero occurrences) - skip pattern[j-1:j]
+            // 2. Use the '*' if preceding character matches - consume string[i]
+            return helper(s, p, i, j - 2) || 
+                   (prevMatch && helper(s, p, i - 1, j));
+        } else {
+            // No '*', so we must match current character and move backward
+            return currentMatch && helper(s, p, i - 1, j - 1);
+        }
     }
 }
