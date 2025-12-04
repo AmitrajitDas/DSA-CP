a71331609479965d00a12636c07661e41e550753
Amitrajit Das <amitrajitdas31@gmail.com>
2025-12-04 22:02:52 +0530

    Time: 0 ms (100%), Space: 46.8 MB (9%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -14,23 +14,18 @@
  * }
  */
 class Solution {
-    public int findBottomLeftValue(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        /*
-        Doing BFS right-to-left means we can simply return the 
-        last node's value and don't have to keep track of the first node 
-        in the current row or even care about rows at all.
-        */
-        while(!q.isEmpty()) {
-            root = q.poll();
-            if(root.right != null) {
-                q.offer(root.right);
-            }
-            if(root.left != null) {
-                q.offer(root.left);
-            }
+    private int maxDepth = -1, res = 0;
+    private void dfs(TreeNode root, int depth) {
+        if(root == null) return;
+        dfs(root.left, depth + 1);
+        if(depth > maxDepth) {
+            maxDepth = depth;
+            res = root.val;
         }
-        return root.val;
+        dfs(root.right, depth + 1);
+    }
+    public int findBottomLeftValue(TreeNode root) {
+        dfs(root, 0);
+        return res;
     }
 }
