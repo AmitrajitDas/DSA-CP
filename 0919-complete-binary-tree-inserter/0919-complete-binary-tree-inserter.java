fab885b45012d24a6c9fc813bdc1e5c14b3c714c
Amitrajit Das <amitrajitdas31@gmail.com>
2026-01-05 00:13:29 +0530

    Time: 15 ms (74.67%), Space: 47 MB (24%) - LeetHub

--- 0919-complete-binary-tree-inserter/0919-complete-binary-tree-inserter.java
+++ 0919-complete-binary-tree-inserter/0919-complete-binary-tree-inserter.java
@@ -15,22 +15,29 @@
  */
 class CBTInserter {
     private TreeNode root;
-    private Deque<TreeNode> nodeQueue;
+    private Deque<TreeNode> nodeQueue; // Queue of nodes that can still accept children
 
     public CBTInserter(TreeNode root) {
         this.root = root;
         this.nodeQueue = new LinkedList<>();
+        
         if (root != null) {
             this.nodeQueue.offer(root);
+            
+            // Traverse the tree to find the first incomplete node
             while (true) {
-                TreeNode node = this.nodeQueue.peek();
+                TreeNode node = this.nodeQueue.peek(); // Look at front node without removing
+                
                 if (node.left != null) {
-                    this.nodeQueue.offer(node.left);
+                    this.nodeQueue.offer(node.left); // Add left child to queue
+                    
                     if (node.right != null) {
-                        this.nodeQueue.offer(node.right);
-                        this.nodeQueue.poll();
+                        this.nodeQueue.offer(node.right); // Add right child to queue
+                        this.nodeQueue.poll(); // Node is complete, remove from queue
                     }
                 }
+                
+                // Stop when we find a node missing at least one child
                 if (node.left == null || node.right == null) {
                     break;
                 }
@@ -39,25 +46,33 @@ class CBTInserter {
     }
     
     public int insert(int val) {
+        // Handle empty tree case
         if (this.root == null) {
             this.root = new TreeNode(val);
             this.nodeQueue.offer(this.root);
             return -1;
         }
-        TreeNode parent = this.nodeQueue.peekFirst();
+        
+        // Get the parent node (first incomplete node in queue)
+        TreeNode parent = this.nodeQueue.peek();
         TreeNode node = new TreeNode(val);
+        
+        // New node will eventually be a parent, so add it to queue
         this.nodeQueue.offer(node);
+        
+        // Insert as left child if empty, otherwise as right child
         if (parent.left == null) {
             parent.left = node;
         } else {
             parent.right = node;
-            this.nodeQueue.pollFirst();
+            this.nodeQueue.poll(); // Parent is now complete, remove from queue
         }
-        return parent.val;
+        
+        return parent.val; // Return parent's value
     }
     
     public TreeNode get_root() {
-        return this.root;
+        return this.root; // Return the root of the tree
     }
 }
 
