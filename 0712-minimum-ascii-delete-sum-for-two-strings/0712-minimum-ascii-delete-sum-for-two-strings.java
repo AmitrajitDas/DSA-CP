f3cf7cb71219ef858a23a6970a7029519e74b9d1
Amitrajit Das <amitrajitdas31@gmail.com>
2026-01-24 01:16:14 +0530

    Time: 30 ms (11.27%), Space: 46.4 MB (81.27%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -4,26 +4,31 @@ class Solution {
         int n = s2.length();
         int[][] dp = new int[m + 1][n + 1];
 
-
-        for (int i = m - 1; i >= 0; i--) {
-            dp[i][n] = dp[i + 1][n] + s1.charAt(i);
+        // Base case: when i = 0 (all of s1 processed)
+        // Need to delete all remaining characters in s2
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = s2.charAt(j - 1) + dp[0][j - 1];
         }
-        for (int j = n - 1; j >= 0; j--) {
-            dp[m][j] = dp[m][j + 1] + s2.charAt(j);
+        
+        // Base case: when j = 0 (all of s2 processed)
+        // Need to delete all remaining characters in s1
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = s1.charAt(i - 1) + dp[i - 1][0];
         }
 
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                if (s1.charAt(i) == s2.charAt(j)) {
-                    dp[i][j] = dp[i + 1][j + 1];
+        // Fill the DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
                 } else {
-                    int delete_s1_i = s1.charAt(i) + dp[i + 1][j];
-                    int delete_s2_j = s2.charAt(j) + dp[i][j + 1];
+                    int delete_s1_i = s1.charAt(i - 1) + dp[i - 1][j];
+                    int delete_s2_j = s2.charAt(j - 1) + dp[i][j - 1];
                     dp[i][j] = Math.min(delete_s1_i, delete_s2_j);
                 }
             }
         }
 
-        return dp[0][0];
+        return dp[m][n];
     }
 }
