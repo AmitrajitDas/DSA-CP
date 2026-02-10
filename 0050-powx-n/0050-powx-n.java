67f7711a01dcc4d07898983fcf1684cb931d6570
Amitrajit Das <amitrajitdas31@gmail.com>
2026-02-10 21:10:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,38 +1,17 @@
-// Time Complexity (TC): O(log n)
-// Space Complexity (SC): O(log n)
-
-// Binary Exponentiation is an efficient algorithm for calculating
-// the power of a number 'x' raised to the power 'n'.
 class Solution {
-    // Helper function to perform binary exponentiation recursively.
-    // The function takes the base 'x' and the exponent 'n'.
     private double solve(double x, long n) {
-        // Base case: if the exponent is 0, return 1 as any number
-        // raised to the power of 0 is 1.
-        if (n == 0) return 1;
+        if(n == 1) return x;
+        if(n < 0) {
+            return solve(1/x, -n);
+        }
+        if(n % 2 == 1) {
+            return x * solve(x*x, n/2);
+        }
 
-        // If the exponent is negative, we use the property:
-        // x^n = 1 / (x^-n). So, we call the solve function with 1/x
-        // as the base and -n as the exponent.
-        if (n < 0) return solve(1 / x, -n);
-
-        // If the exponent is even, we use the property:
-        // x^n = (x^2)^(n/2). So, we call the solve function with
-        // x*x as the base and n/2 as the exponent.
-        if (n % 2 == 0) return solve(x * x, n / 2);
-
-        // If the exponent is odd, we use the property:
-        // x^n = x * (x^2)^(n/2). So, we call the solve function with
-        // x*x as the base and n/2 as the exponent, and multiply the
-        // result with 'x'.
-        return x * solve(x * x, n / 2);
+        return solve(x*x, n/2);
     }
 
-    // Main function to calculate x raised to the power of n.
     public double myPow(double x, int n) {
-        // We convert the exponent 'n' to a long type to handle
-        // cases where 'n' is Integer.MIN_VALUE, which would cause
-        // overflow if not converted to long.
-        return solve(x, (long) n);
+        return solve(x, (long)n);
     }
 }
