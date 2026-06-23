baed5472548b386158a319fb762bdd9bd0df562b
Amitrajit Das <amitrajitdas31@gmail.com>
2026-06-23 12:56:15 +0530

    Time: 0 ms (100%), Space: 44.1 MB (74.06%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,18 +1,18 @@
 class Solution {
     private void backtrack(int start, List<Integer> curr, int[] nums, List<List<Integer>> res) {
+        // snapshot current subset at every node (not just leaves)
         res.add(new ArrayList<>(curr));
 
-        for(int i = start; i < nums.length; i++) {
-            curr.add(nums[i]);
-            backtrack(i + 1, curr, nums, res);
-            curr.remove(curr.size() - 1);
+        for (int i = start; i < nums.length; i++) {
+            curr.add(nums[i]);              // choose
+            backtrack(i + 1, curr, nums, res);  // explore (i+1 to avoid re-picking same element)
+            curr.remove(curr.size() - 1);   // unchoose
         }
     }
 
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        List<Integer> curr = new ArrayList<>();
-        backtrack(0, curr, nums, res);
+        backtrack(0, new ArrayList<>(), nums, res);
         return res;
     }
 }
