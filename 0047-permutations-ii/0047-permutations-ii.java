3f0c1213be34234a8ca2e115e97f0329e25ff7d9
Amitrajit Das <amitrajitdas31@gmail.com>
2026-06-24 19:11:12 +0530

    Time: 3 ms (90.11%), Space: 46.1 MB (99.05%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,5 +1,4 @@
 class Solution {
-    // Explores all possible permutations by choosing unused elements one at a time
     private void backtrack(List<Integer> curr, boolean[] used, List<List<Integer>> res, int[] nums) {
         // Base case: current permutation is complete
         if(curr.size() == nums.length) {
@@ -10,7 +9,6 @@ class Solution {
             // Skip already used elements
             if(used[i]) continue;
             if (i > 0 && nums[i] == nums[i-1] && !used[i-1]) continue;
-
             // Choose: mark element as used and add to current permutation
             used[i] = true;
             curr.add(nums[i]);
@@ -23,11 +21,12 @@ class Solution {
             used[i] = false;
         }
     }
+
     public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> curr = new ArrayList<>();
         boolean[] used = new boolean[nums.length]; // defaults to false
-        Arrays.sort(nums);
         backtrack(curr, used, res, nums);
         return res;
     }
