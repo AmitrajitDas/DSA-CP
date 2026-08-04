d44732f79a62073e8aa8c196ec16758545e2dfee
Amitrajit Das <amitrajitdas31@gmail.com>
2026-08-04 16:31:07 +0530

    Time: 1 ms (33.28%), Space: 46.3 MB (95.58%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -15,19 +15,19 @@
  */
 class Solution {
     int maxSum = Integer.MIN_VALUE;
-    private int calcSum(TreeNode root) {
+    
+    private int recurse(TreeNode root) {
         if(root == null) return 0;
-        int leftSum = Math.max(0, calcSum(root.left));
-        int rightSum = Math.max(0, calcSum(root.right));
 
-        maxSum = Math.max(maxSum, root.val + leftSum + rightSum);
-        System.out.println("maxSum: " + maxSum); 
-        System.out.println("return val: " + (root.val + Math.max(leftSum, rightSum)));
-        return root.val + Math.max(leftSum, rightSum);
+        int leftVal = Math.max(0, recurse(root.left));
+        int rightVal = Math.max(0, recurse(root.right));
+        int currSum = root.val + leftVal + rightVal;
+        maxSum = Math.max(currSum, maxSum);
+        return root.val + Math.max(leftVal, rightVal);
     }
 
     public int maxPathSum(TreeNode root) {
-        calcSum(root);
+        recurse(root);
         return maxSum;
     }
 }
